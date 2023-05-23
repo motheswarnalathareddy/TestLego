367d2df62e6439c37d6cc3d4ac5baedde8412eac
motheswarnalathareddy <113678205+motheswarnalathareddy@users.noreply.github.com>
2023-05-23 15:51:26 +0200

    Add files via upload

---
+++ src/test/java/testCases/BaseSetup.java
@@ -0,0 +1,26 @@
+package testCases;
+
+import utilities.BrowserFactory;
+import utilities.ConfigDataProvider;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseSetup {
+
+    public WebDriver driver;
+    public ConfigDataProvider config;
+    @BeforeSuite
+    public void setSuite(){
+        config=new ConfigDataProvider();
+
+    }
+    @BeforeClass
+    public void init() {
+        driver= BrowserFactory.startApplication(driver, config.getBrowser(), config.getUrl());
+   }
+//    @AfterClass
+//    public void teardown() {
+//        BrowserFactory.quitBrowser(driver);
+//    }
+}
---
+++ src/test/java/testCases/ProductBasketValidationTest.java
@@ -0,0 +1,18 @@
+package testCases;
+
+import pageObjects.AddProductToCartPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+public class ProductBasketValidationTest extends BaseSetup {
+
+    @Test
+    public void VerifyProductBasket(){
+        AddProductToCartPage addProductToCartPage =PageFactory.initElements(driver, AddProductToCartPage.class);
+        addProductToCartPage.VerifyProductBasket(config.getProductName());
+        Assert.assertTrue(driver.findElement(By.xpath("//h1")).getText().contains("My Bag"));
+    }
+}
